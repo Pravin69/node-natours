674131f4991f2806baf355be9cafa091abfa1263
pravin69 <p.thakur.133.ram@gmail.com>
2023-10-19 23:26:50 +0530

    Message alert bug2 fixed

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -78,7 +78,7 @@ exports.createBookingCheckout = async (session) => {
 
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].unit_amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
